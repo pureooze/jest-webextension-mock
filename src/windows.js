060992f486a5e12dc5dc33c2e2441d9d03ed0595
Uzair Shamim <uzashamim@gmail.com>
2018-09-07 11:47:19 -0400

    API update

--- src/windows.js
+++ src/windows.js
@@ -4,8 +4,8 @@ export const windows = {
   get: jest.fn((id = '', getInfo = {}, cb = () => {}) => cb({})),
   getCurrent: jest.fn((getInfo = {}, cb = () => {}) => cb({})),
   getLastFocused: jest.fn((getInfo = {}, cb = () => {}) => cb({})),
-  getAll: jest.fn((getInfo = {}, cb = () => {}) => cb({})),
+  getAll: jest.fn((getInfo = {}, cb = () => {}) => cb([{}])),
   create: jest.fn((createData = {}, cb = () => {}) => cb({})),
   update: jest.fn((windowId = '', updateInfo = {}, cb = () => {}) => cb({})),
-  remove: jest.fn((windowId = '', cb = () => {}) => cb({})),
+  remove: jest.fn((windowId = '', cb = () => {}) => cb()),
 };
